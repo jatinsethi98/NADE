9012df84b8fa8d29e8372b0b73de5fef7cf542b5
Jatin Sethi <jatinsethi98@gmail.com>
2026-08-15 04:00:30 -0400

    Initial Commit

---
+++ NADE/NADEApp.swift
@@ -0,0 +1,17 @@
+//
+//  NADEApp.swift
+//  NADE
+//
+//  Created by Jatin Sethi on 8/15/26.
+//
+
+import SwiftUI
+
+@main
+struct NADEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
